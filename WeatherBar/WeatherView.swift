c753fd5604148a56e33045b7fcc0122a9f2a5603
Dinis <dinispeixoto96@gmail.com>
2018-02-24 21:46:46 +0000

    weather view

---
+++ WeatherBar/WeatherView.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherView.swift
+//  WeatherBar
+//
+//  Created by Dinis Peixoto on 24/02/2018.
+//  Copyright © 2018 Dinis Peixoto. All rights reserved.
+//
+
+import Cocoa
+
+class WeatherView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
